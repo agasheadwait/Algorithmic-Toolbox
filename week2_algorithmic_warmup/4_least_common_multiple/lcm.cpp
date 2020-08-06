edcb8191a43d3362ea7a1a319f8df476083af1a7
agasheadwait <agasheadwait24@gmail.com>
2020-08-06 19:32:43 +0530

    Week 2 Solutions

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 long long lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
@@ -8,9 +9,23 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long long int gcd_fast(long long int a,long long int b){
+  if(a==0){
+    return b;
+  }
+  if(b==0){
+    return a;
+  }
+  long long int ma = max(a,b);
+  long long int mi = min(a,b);
+  ma = ma%mi;
+  return gcd_fast(mi,ma);
+
+}
+
 int main() {
-  int a, b;
+  long long int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << a*b/gcd_fast(a, b) << std::endl;
   return 0;
 }
