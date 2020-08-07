8012e9b6c3c173f6395d221d5b5cb2474e3e7545
agasheadwait <agasheadwait24@gmail.com>
2020-08-07 18:19:07 +0530

    Week 2 Solutions

--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -17,9 +17,49 @@ int fibonacci_sum_squares_naive(long long n) {
 
     return sum % 10;
 }
+long long pisano(long long m){
+    long long prev=0,curr=1;
+    long long temp;
+    for(long long i=0;i<m*m;i++){
+        temp = prev;
+        prev = curr;
+        curr =  (temp+curr)%m;
+        if(prev==0 && curr==1){
+            return i+1;
+        }
+    }
+}
+
+
+long long get_fibonacci_huge_naive(long long n, long long m) {
+    long long pisanop = pisano(m);
+    n = n%pisanop;
+    long long prev=0,curr=1;
+    if (n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    for(long long i=0;i<n-1;i++){
+        curr=curr+prev;
+        prev = curr-prev;
+        curr=curr%m;
+        prev=prev%m;
+        
+    }
+    return curr%m;
+
+
+}
+
+int fibonacci_sum_squares_fast(long long n){
+    return (get_fibonacci_huge_naive(n,10)*get_fibonacci_huge_naive(n+1,10))%10;
+}
+
 
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << fibonacci_sum_squares_fast(n);
 }
