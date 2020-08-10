77dc11be63e939890a8fd138bf4c37bf98219c7e
agasheadwait <agasheadwait24@gmail.com>
2020-08-10 18:15:23 +0530

    Week 3 Complete

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,23 +1,43 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h> 
 
-using std::vector;
+using namespace std;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+bool comp(pair <double,pair<int,int>>p1,pair <double,pair<int,int>>p2){
+  return p1.first>p2.first;
+}
+
+double get_optimal_value(double capacity, vector<double> w, vector<double> v) {
   double value = 0.0;
-
-  // write your code here
+  vector< pair <double,pair<double,double>> > ans;
+  for(double i=0;i<w.size();i++){
+    ans.push_back(make_pair(v[i]/w[i],make_pair(v[i],w[i])));
+  }
+  sort(ans.begin(),ans.end(),comp);
+  while(capacity>0 && ans.size()>0){
+    if(ans[0].second.second<=capacity){
+      value+=ans[0].second.first;
+      capacity-=ans[0].second.second;
+    }
+    else
+    {
+      value+=ans[0].first*capacity; 
+      capacity=0;
+    }
+    ans.erase(ans.begin());
+    
+  }
+  
 
   return value;
 }
 
 int main() {
-  int n;
-  int capacity;
+  double n;
+  double capacity;
   std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
-  for (int i = 0; i < n; i++) {
+  vector<double> values(n);
+  vector<double> weights(n);
+  for (double i = 0; i < n; i++) {
     std::cin >> values[i] >> weights[i];
   }
 
