7b4a93464010c468d1902e481d14f3a2f730189c
agasheadwait <agasheadwait24@gmail.com>
2020-08-10 17:54:15 +0530

    Week 3 Complete

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include<cmath>
 
 int get_change(int m) {
-  //write your code here
-  return n;
+  int n=0;
+  n+=floor(m/10);
+  m = m%10;
+  n+=floor(m/5);
+  m = m%5;
+  return n+m;
 }
 
 int main() {
