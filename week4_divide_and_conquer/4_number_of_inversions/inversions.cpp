8b02ca4d705187b0408cc9b7cabf868ea494e323
agasheadwait <agasheadwait24@gmail.com>
2020-08-19 10:17:23 +0530

    Week 4 Solutions

--- week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
+
+int merge(vector<int>&a, vector<int>&b, int l, int mid, int r){
+  int number_of_inversions = 0;
+  int i=l,k=l,j=mid;
+  while((i<mid)&&(j<=r)){
+    if(a[i]<=a[j]){
+      b[k++] = a[i++];
+    }
+    else{
+      b[k++] = a[j++];
+      number_of_inversions+=mid-i;
+    }
+  }
+  while(i<mid){
+    b[k++] = a[i++];
+  }
+  while(j<r+1){
+    b[k++] = a[j++];
+  }
+  for(i=l;i<=r;i++){
+    a[i] = b[i];
+  }
+  return number_of_inversions;
+
+}
 
 int get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
   int number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
+  if (right <= left) return number_of_inversions;
+  int mid = left + (right - left) / 2;
+  number_of_inversions += get_number_of_inversions(a, b, left, mid);
+  number_of_inversions += get_number_of_inversions(a, b, mid+1, right);
+  number_of_inversions += merge(a,b,left,mid+1,right);
   return number_of_inversions;
 }
 
@@ -21,5 +46,5 @@ int main() {
     std::cin >> a[i];
   }
   vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+  std::cout << get_number_of_inversions(a, b, 0, a.size()-1) << '\n';
 }
