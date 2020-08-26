bbc80f39d51fdd51b7570293c1b884d157cec92e
agasheadwait <agasheadwait24@gmail.com>
2020-08-26 10:37:20 +0530

    Week 6 Final

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
+using std::map;
 using std::vector;
+using std::max;
 
-int optimal_weight(int W, const vector<int> &w) {
+int optimal_weight(int W, const vector<int> &w)
+{
   //write your code here
   int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
+  for (size_t i = 0; i < w.size(); ++i)
+  {
+    if (current_weight + w[i] <= W)
+    {
       current_weight += w[i];
     }
   }
   return current_weight;
 }
 
-int main() {
+int optimal_weight_dp(int W, const vector<int> &weights)
+{
+vector<vector<int>> value(weights.size() + 1, vector<int>(W + 1, 0));  for (int i = 1; i <= weights.size(); i++)
+  {
+    for (int w = 1; w <= W; w++)
+    {
+      value[i][w] = value[i - 1][w];
+      if (weights[i - 1] <= w)
+      {
+        int val = value[i-1][w - weights[i-1]] + weights[i-1];
+        value[i][w] = max(val,value[i][w]);
+      }
+    }
+  }
+  return value[weights.size()][W];
+}
+
+int main()
+{
   int n, W;
   std::cin >> W >> n;
   vector<int> w(n);
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << optimal_weight_dp(W, w) << '\n';
 }
--- week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
+++ week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
 
-int partition3(vector<int> &A) {
-  //write your code here
-  return 0;
+int partition3(vector<int> &A)
+{
+  if (accumulate(A.begin(), A.end(), 0) % 3 == 0)
+  {
+    if(A.size()<3){
+      return 0;
+    }
+    
+  }
+  else
+  {
+    return 0;
+  }
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> A(n);
-  for (size_t i = 0; i < A.size(); ++i) {
+  for (size_t i = 0; i < A.size(); ++i)
+  {
     std::cin >> A[i];
   }
   std::cout << partition3(A) << '\n';
